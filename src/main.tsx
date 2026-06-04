7225513d88e8de879ee1be72947140dd0e78a626
Todd Tomlinson <tt@usicna.com>
2026-06-04 22:18:56 +0000

    feat: Integrate AI Quote Chat Widget via Vite bundling

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+// Import the widget script into the main entry point for Vite to bundle it
+import './widget.js'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
